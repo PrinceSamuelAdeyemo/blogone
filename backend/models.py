4e76464a6f1e5f35fe7601fa313ab6e4ae0c8bbe
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2024-02-03 19:13:22 +0100

    minor changes

--- backend/models.py
+++ backend/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
 class Author(models.Model):
     name = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    author_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
 
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name = ("Author")
+        verbose_name_plural = ("Authors")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return reverse("author_detail", kwargs={"pk": self.pk})
 
 class Article(models.Model):
-    article_name = models.CharField(max_length=100, blank=false, null=false)
+    article_name = models.CharField(max_length=100, blank=False, null=False)
+    article_name = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    #models.SlugField((""))
     
     def  __str__(self):
         return self.article_name
+    
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.pk})
+    
     
     
